37e68f9ea8d7deb102966f8a44da600aadc5a87f
yfayed2006 <yfayed2006@gmail.com>
2026-03-13 21:19:28 +0300

    Create __manifest__.py

---
+++ addons/route_core/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Route Core',
+    'version': '1.0',
+    'summary': 'Core module for route distribution',
+    'author': 'Yasser',
+    'category': 'Sales',
+    'depends': ['contacts', 'stock'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
