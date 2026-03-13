a73174bca7d3694588127a42903082d90470e49d
yfayed2006 <yfayed2006@gmail.com>
2026-03-13 22:37:29 +0300

    Create route.py

---
+++ addons/route_core/models/route.py
@@ -0,0 +1,35 @@
+from odoo import models, fields, api
+
+
+class RouteRoute(models.Model):
+    _name = 'route.route'
+    _description = 'Route'
+    _order = 'name'
+
+    name = fields.Char(string='Route Name', required=True)
+    code = fields.Char(string='Route Code', copy=False, readonly=True)
+    active = fields.Boolean(string='Active', default=True)
+
+    user_id = fields.Many2one(
+        'res.users',
+        string='Salesperson'
+    )
+
+    visit_day = fields.Selection([
+        ('sat', 'Saturday'),
+        ('sun', 'Sunday'),
+        ('mon', 'Monday'),
+        ('tue', 'Tuesday'),
+        ('wed', 'Wednesday'),
+        ('thu', 'Thursday'),
+        ('fri', 'Friday'),
+    ], string='Visit Day')
+
+    notes = fields.Text(string='Notes')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if not vals.get('code'):
+                vals['code'] = self.env['ir.sequence'].next_by_code('route.route') or 'NEW'
+        return super().create(vals_list)
