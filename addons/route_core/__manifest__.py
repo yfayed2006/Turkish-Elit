54b2297872940df2fb8ac55d9c54796fe94f911d
yfayed2006 <yfayed2006@gmail.com>
2026-03-14 20:09:39 +0300

    Create __manifest__.py

---
+++ addons/route_core/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Route Core",
+    "version": "17.0.1.0.0",
+    "summary": "Route system base module",
+    "description": "Base module for route system",
+    "author": "Yasser Fayed",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": True,
+}
