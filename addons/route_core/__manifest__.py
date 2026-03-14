6c79264942418c553850ad0b66acc2727a26d73c
yfayed2006 <yfayed2006@gmail.com>
2026-03-14 20:28:40 +0300

    Update __manifest__.py

--- addons/route_core/__manifest__.py
+++ addons/route_core/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Route Core",
-    "version": "17.0.1.0.0",
+    "version": "19.0.1.0.0",
     "summary": "Route system base module",
     "description": "Base module for route system",
     "author": "Yasser Fayed",
