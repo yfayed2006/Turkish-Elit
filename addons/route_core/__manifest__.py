9c0df69df90f4fb5c1fa6240e9b8984b7c032d5d
yfayed2006 <yfayed2006@gmail.com>
2026-03-15 02:42:25 +0300

    Update __manifest__.py

--- addons/route_core/__manifest__.py
+++ addons/route_core/__manifest__.py
@@ -5,6 +5,7 @@
     "depends": ["base", "sale", "mail"],
     "data": [
         "security/ir.model.access.csv",
+        "data/route_visit_sequence.xml",
         "views/route_visit_views.xml",
         "wizard/route_visit_end_wizard_views.xml",
     ],
