7865c3cd6ed62fd947e8c56fd7f028dba29732cd
yfayed2006 <yfayed2006@gmail.com>
2026-03-15 02:17:29 +0300

    Update sale_order.py

--- addons/route_core/models/sale_order.py
+++ addons/route_core/models/sale_order.py
@@ -4,17 +4,17 @@ from odoo import models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def action_confirm(self):
-        result = super().action_confirm()
+    def button_confirm(self):
+        result = super().button_confirm()
 
         route_visit_id = self.env.context.get("route_visit_id")
-        if route_visit_id:
+        if route_visit_id and len(self) == 1:
             return {
                 "type": "ir.actions.act_window",
                 "name": "Route Visit",
                 "res_model": "route.visit",
-                "view_mode": "form",
                 "res_id": route_visit_id,
+                "view_mode": "form",
                 "target": "current",
             }
 
