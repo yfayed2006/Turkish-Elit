80d94b7afc19ab18cc2e7cdfae1041533fe15fe0
yfayed2006 <yfayed2006@gmail.com>
2026-03-14 21:17:56 +0300

    Update route_visit.py

--- addons/route_core/models/route_visit.py
+++ addons/route_core/models/route_visit.py
@@ -21,3 +21,19 @@ class RouteVisit(models.Model):
     )
     user_id = fields.Many2one("res.users", string="Salesperson")
     partner_id = fields.Many2one("res.partner", string="Customer")
+
+    def action_start_visit(self):
+        for rec in self:
+            rec.state = "in_progress"
+
+    def action_end_visit(self):
+        for rec in self:
+            rec.state = "done"
+
+    def action_cancel_visit(self):
+        for rec in self:
+            rec.state = "cancel"
+
+    def action_reset_to_draft(self):
+        for rec in self:
+            rec.state = "draft"
