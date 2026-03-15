f0bf7c0d31a3dfd300c15e2a69e640b6cf266cb5
yfayed2006 <yfayed2006@gmail.com>
2026-03-15 15:44:10 +0300

    Update __manifest__.py

--- addons/route_core/__manifest__.py
+++ addons/route_core/__manifest__.py
@@ -2,10 +2,12 @@
     "name": "Route Core",
     "version": "19.0.1.0.0",
     "summary": "Sales representative route visits",
-    "depends": ["base", "sale", "mail"],
+    "depends": ["base", "sale", "mail", "stock", "contacts"],
     "data": [
         "security/ir.model.access.csv",
         "data/route_visit_sequence.xml",
+        "views/route_area_views.xml",
+        "views/res_partner_views.xml",
         "views/route_visit_views.xml",
         "wizard/route_visit_end_wizard_views.xml",
     ],
