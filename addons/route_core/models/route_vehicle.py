df740b06dc8d2f6b5e3ad5dba8620854557e8355
yfayed2006 <yfayed2006@gmail.com>
2026-03-15 18:40:25 +0300

    Update route_vehicle.py

--- addons/route_core/models/route_vehicle.py
+++ addons/route_core/models/route_vehicle.py
@@ -27,11 +27,54 @@ class RouteVehicle(models.Model):
         string="Notes",
     )
 
+    visit_ids = fields.One2many(
+        "route.visit",
+        "vehicle_id",
+        string="Visits",
+    )
+    visit_count = fields.Integer(
+        string="Visits Count",
+        compute="_compute_visit_stats",
+    )
+    last_visit_id = fields.Many2one(
+        "route.visit",
+        string="Last Visit",
+        compute="_compute_visit_stats",
+    )
+    last_visit_date = fields.Date(
+        string="Last Visit Date",
+        compute="_compute_visit_stats",
+    )
+    last_sale_order_id = fields.Many2one(
+        "sale.order",
+        string="Last Sale Order",
+        compute="_compute_visit_stats",
+    )
+
     _sql_constraints = [
         ("route_vehicle_name_unique", "unique(name)", "Vehicle name must be unique."),
         ("route_vehicle_code_unique", "unique(code)", "Vehicle code must be unique."),
         ("route_vehicle_plate_no_unique", "unique(plate_no)", "Plate number must be unique."),
     ]
+
+    @api.depends(
+        "visit_ids",
+        "visit_ids.date",
+        "visit_ids.sale_order_id",
+        "visit_ids.write_date",
+    )
+    def _compute_visit_stats(self):
+        for record in self:
+            visits = record.visit_ids.sorted(
+                key=lambda v: ((v.date or fields.Date.today()), v.id),
+                reverse=True,
+            )
+            record.visit_count = len(visits)
+            record.last_visit_id = visits[0] if visits else False
+            record.last_visit_date = visits[0].date if visits else False
+
+            last_visit_with_sale = next((visit for visit in visits if visit.sale_order_id), False)
+            record.last_sale_order_id = last_visit_with_sale.sale_order_id if last_visit_with_sale else False
 
     @api.model_create_multi
     def create(self, vals_list):
