68e6f756ab4ede0f409912aa96899b04fb8e5c51
yfayed2006 <yfayed2006@gmail.com>
2026-03-16 16:38:03 +0300

    Update route_plan_pda.py

--- addons/route_core/models/route_plan_pda.py
+++ addons/route_core/models/route_plan_pda.py
@@ -6,11 +6,19 @@ class RoutePlan(models.Model):
 
     def action_open_pda_screen(self):
         self.ensure_one()
+
+        visits = self.line_ids.mapped("visit_id")
+
         action = self.env.ref("route_core.action_route_visit_pda").read()[0]
-        action["domain"] = [("plan_id", "=", self.id)]
+        action["domain"] = [("id", "in", visits.ids)]
         action["context"] = {
-            "default_plan_id": self.id,
+            "search_default_filter_my_visits": 0,
+            "search_default_filter_today": 0,
+            "default_user_id": self.user_id.id,
+            "default_date": self.date,
             "pda_mode": True,
+            "create": 0,
+            "delete": 0,
         }
         action["name"] = f"PDA - {self.display_name}"
         return action
