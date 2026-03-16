1e362b2aa71588b1f02cc9b529efd29ab8609a24
yfayed2006 <yfayed2006@gmail.com>
2026-03-16 21:06:20 +0300

    Update route_vehicle_location_ext.py

--- addons/route_core/models/route_vehicle_location_ext.py
+++ addons/route_core/models/route_vehicle_location_ext.py
@@ -11,3 +11,11 @@ class RouteVehicle(models.Model):
         ondelete="restrict",
         help="Internal stock location used as the main van location for this vehicle.",
     )
+
+    def get_stock_location(self):
+        self.ensure_one()
+        return self.stock_location_id
+
+    def has_stock_location(self):
+        self.ensure_one()
+        return bool(self.stock_location_id)
