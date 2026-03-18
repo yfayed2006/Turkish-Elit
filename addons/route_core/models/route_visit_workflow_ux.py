5860bc21b8e3fb7bbed2984869c53e7c12e4dac0
yfayed2006 <yfayed2006@gmail.com>
2026-03-18 16:30:45 +0300

    Update route_visit_workflow_ux.py

--- addons/route_core/models/route_visit_workflow_ux.py
+++ addons/route_core/models/route_visit_workflow_ux.py
@@ -156,8 +156,8 @@ class RouteVisit(models.Model):
             elif rec.visit_process_state == "counting" and not rec.returns_step_done:
                 rec.ux_stage = "returns"
                 rec.ux_primary_action = "returns_step"
-                rec.ux_stage_title = "Returns step"
-                rec.ux_stage_help = "Either scan returns or choose No Returns."
+                rec.ux_stage_title = "Check returns"
+                rec.ux_stage_help = "Are there any returns in this visit? Choose No Returns or Yes, Scan Returns."
 
             elif rec.visit_process_state == "counting" and rec.returns_step_done:
                 rec.ux_stage = "reconcile"
