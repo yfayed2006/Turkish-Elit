53b4b2eec85b48d2eb4e5308c21aef11bee9c9ea
yfayed2006 <yfayed2006@gmail.com>
2026-03-22 13:28:41 +0300

    Update __manifest__.py

--- addons/route_core/__manifest__.py
+++ addons/route_core/__manifest__.py
@@ -23,6 +23,7 @@
         "views/route_visit_payment_ux_views.xml",
         "views/outlet_stock_balance_views.xml",
         "views/route_location_link_views.xml",
+        "views/route_product_barcode_views.xml",
         "wizard/route_visit_end_wizard_views.xml",
         "wizard/route_plan_add_area_outlets_wizard_views.xml",
         "wizard/route_visit_scan_wizard_views.xml",
