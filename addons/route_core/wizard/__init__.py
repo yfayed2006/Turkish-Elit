5bc07b3e361697e68ace41a2b4d8ec30624cd40b
yfayed2006 <yfayed2006@gmail.com>
2026-03-25 01:47:11 +0300

    Update __init__.py

--- addons/route_core/wizard/__init__.py
+++ addons/route_core/wizard/__init__.py
@@ -3,3 +3,6 @@ from . import route_plan_add_area_outlets_wizard
 from . import route_visit_scan_wizard
 from . import route_visit_return_scan_wizard
 from . import route_visit_collect_payment_wizard
+
+from . import route_visit_missing_lot_wizard
+
