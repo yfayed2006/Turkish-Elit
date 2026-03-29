9f5f2699283cbaedbf0a3f43db675ff4004fc03b
yfayed2006 <yfayed2006@gmail.com>
2026-03-29 14:19:47 +0300

    Update route_area.py

--- addons/route_core/models/route_area.py
+++ addons/route_core/models/route_area.py
@@ -1,10 +1,10 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class RouteArea(models.Model):
     _name = "route.area"
     _description = "Route Area"
-    _order = "name"
+    _order = "country_id, city_id, name"
 
     name = fields.Char(
         string="Area Name",
@@ -12,6 +12,20 @@ class RouteArea(models.Model):
     )
     code = fields.Char(
         string="Code",
+    )
+    city_id = fields.Many2one(
+        "route.city",
+        string="City",
+        ondelete="restrict",
+        index=True,
+    )
+    country_id = fields.Many2one(
+        "res.country",
+        string="Country",
+        related="city_id.country_id",
+        store=True,
+        readonly=True,
+        index=True,
     )
     active = fields.Boolean(
         default=True,
@@ -40,10 +54,12 @@ class RouteArea(models.Model):
         compute="_compute_outlet_count",
     )
 
+    @api.depends("visit_ids")
     def _compute_visit_count(self):
         for rec in self:
             rec.visit_count = len(rec.visit_ids)
 
+    @api.depends("outlet_ids")
     def _compute_outlet_count(self):
         for rec in self:
             rec.outlet_count = len(rec.outlet_ids)
@@ -52,14 +68,20 @@ class RouteArea(models.Model):
         self.ensure_one()
         action = self.env.ref("route_core.action_route_visit").read()[0]
         action["domain"] = [("area_id", "=", self.id)]
-        action["context"] = dict(self.env.context, default_area_id=self.id)
+        action["context"] = dict(
+            self.env.context,
+            default_area_id=self.id,
+        )
         return action
 
     def action_view_outlets(self):
         self.ensure_one()
         action = self.env.ref("route_core.action_route_outlet").read()[0]
         action["domain"] = [("area_id", "=", self.id)]
-        action["context"] = dict(self.env.context, default_area_id=self.id)
+        action["context"] = dict(
+            self.env.context,
+            default_area_id=self.id,
+        )
         return action
 
     def action_save_and_back(self):
