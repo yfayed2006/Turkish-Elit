547c6fece082a18d8c01c7fe88855d06b136e131
yfayed2006 <yfayed2006@gmail.com>
2026-03-29 14:32:01 +0300

    Update __manifest__.py

--- addons/route_core/__manifest__.py
+++ addons/route_core/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Route Core",
-    "version": "19.0.1.0.7",
+    "version": "19.0.1.0.8",
     "summary": "Sales representative route visits",
     "author": "Yasser Fayed",
     "license": "LGPL-3",
@@ -17,6 +17,7 @@
         "data/route_vehicle_closing_sequence.xml",
         "data/route_salesperson_shortage_sequence.xml",
         "data/route_salesperson_deduction_sequence.xml",
+        "views/route_city_views.xml",
         "views/route_area_views.xml",
         "views/route_outlet_views.xml",
         "views/route_plan_views.xml",
