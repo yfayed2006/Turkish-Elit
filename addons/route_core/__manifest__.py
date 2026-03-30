537eb0787a4f80025615b18c9127a5b100da0a66
yfayed2006 <yfayed2006@gmail.com>
2026-03-31 01:08:19 +0300

    Update __manifest__.py

--- addons/route_core/__manifest__.py
+++ addons/route_core/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Route Core",
-    "version": "19.0.1.0.18",
+    "version": "19.0.1.0.19",
     "summary": "Sales representative route visits",
     "author": "Yasser Fayed",
     "license": "LGPL-3",
