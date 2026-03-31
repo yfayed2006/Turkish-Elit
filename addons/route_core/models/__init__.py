7dc4b41abdfc78ffb77e68ff8f41a3e86be7f1d7
yfayed2006 <yfayed2006@gmail.com>
2026-03-31 18:03:17 +0300

    Update __init__.py

--- addons/route_core/models/__init__.py
+++ addons/route_core/models/__init__.py
@@ -45,3 +45,6 @@ from . import route_salesperson_shortage
 from . import route_salesperson_deduction
 
 from . import route_pda_home
+
+from . import route_direct_return
+from . import stock_picking_direct_return_ext
