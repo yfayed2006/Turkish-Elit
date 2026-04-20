8b5bd1ac425f741c63202301646bac376e6c7c5b
yfayed2006 <yfayed2006@gmail.com>
2026-04-20 22:07:19 +0300

    Update __init__.py

--- addons/route_core/__init__.py
+++ addons/route_core/__init__.py
@@ -1,18 +1,2 @@
-from . import route_visit_end_wizard
-from . import route_plan_add_area_outlets_wizard
-from . import route_plan_skip_visit_wizard
-from . import route_visit_scan_wizard
-from . import route_visit_return_scan_wizard
-from . import route_visit_collect_payment_wizard
-from . import route_visit_missing_lot_wizard
-from . import route_loading_source_wizard
-from . import route_vehicle_closing_scan_wizard
-from . import route_vehicle_closing_pending_visit_wizard
-from . import route_plan_pending_visit_review_wizard
-
-from . import route_visit_finish_summary_wizard
-
-from . import route_visit_statement_wizard
-
-from . import route_schedule_stop_wizard
-
+from . import models
+from . import wizard
