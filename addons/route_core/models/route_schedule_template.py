f334530f7862b8c2501cef9b0e86b8c0a81d8c79
yfayed2006 <yfayed2006@gmail.com>
2026-04-20 13:11:41 +0300

    Update route_schedule_template.py

--- addons/route_core/models/route_schedule_template.py
+++ addons/route_core/models/route_schedule_template.py
@@ -368,16 +368,28 @@ class RouteScheduleTemplateLine(models.Model):
         self.ensure_one()
         weekday = self._get_effective_weekday()
         sibling_lines = self.env["route.schedule.template.line"]
+        real_id = self._origin.id or (self.id if isinstance(self.id, int) else False)
+
         if self.template_id and self.template_id.id:
-            sibling_lines |= self.search([
+            domain = [
                 ("template_id", "=", self.template_id.id),
                 ("weekday", "=", weekday),
-                ("id", "!=", self.id),
                 ("outlet_id", "!=", False),
-            ])
-        sibling_lines |= self._get_parent_lines().filtered(
-            lambda line: line.id != self.id and line.outlet_id and (line.weekday or "monday") == weekday
+            ]
+            if real_id:
+                domain.append(("id", "!=", real_id))
+            sibling_lines |= self.search(domain)
+
+        parent_lines = self._get_parent_lines().filtered(
+            lambda line: line.outlet_id and (line.weekday or "monday") == weekday
         )
+        if self in parent_lines:
+            parent_lines -= self
+        if self._origin and self._origin in parent_lines:
+            parent_lines -= self._origin
+        if real_id:
+            parent_lines = parent_lines.filtered(lambda line: line.id != real_id)
+        sibling_lines |= parent_lines
         return sibling_lines
 
     def _get_duplicate_line(self):
@@ -402,16 +414,22 @@ class RouteScheduleTemplateLine(models.Model):
             ("id", "!=", self.id),
         ], limit=1)
 
+    def _get_available_outlet_ids(self):
+        self.ensure_one()
+        Outlet = self.env["route.outlet"]
+        if not self.area_id:
+            return []
+        allowed_outlets = Outlet.search([("area_id", "=", self.area_id.id)])
+        used_outlets = self._get_same_day_sibling_lines().mapped("outlet_id")
+        available_outlets = allowed_outlets - used_outlets
+        if self.outlet_id and self.outlet_id.area_id == self.area_id:
+            available_outlets |= self.outlet_id
+        return available_outlets.ids
+
     def _get_available_outlet_domain(self):
         self.ensure_one()
-        used_outlet_ids = self._get_same_day_sibling_lines().mapped("outlet_id").ids
-        if not self.area_id:
-            domain = [("id", "=", 0)]
-        else:
-            domain = [("area_id", "=", self.area_id.id)]
-        if used_outlet_ids:
-            domain.append(("id", "not in", used_outlet_ids))
-        return domain
+        available_ids = self._get_available_outlet_ids()
+        return [("id", "in", available_ids)] if available_ids else [("id", "=", 0)]
 
     def _get_dynamic_domains(self):
         self.ensure_one()
@@ -435,7 +453,7 @@ class RouteScheduleTemplateLine(models.Model):
         for rec in self:
             used_outlet_ids = rec._get_same_day_sibling_lines().mapped("outlet_id").ids
             rec.same_day_used_outlet_ids = Outlet.browse(used_outlet_ids)
-            rec.available_outlet_ids = Outlet.search(rec._get_available_outlet_domain())
+            rec.available_outlet_ids = Outlet.browse(rec._get_available_outlet_ids())
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -503,23 +521,6 @@ class RouteScheduleTemplateLine(models.Model):
         self.ensure_one()
         response = {"domain": self._get_dynamic_domains()}
         if not self.outlet_id:
-            return response
-
-        duplicate_outlet_ids = self._get_same_day_sibling_lines().mapped("outlet_id").ids
-        if self.outlet_id.id in duplicate_outlet_ids:
-            outlet_name = self.outlet_id.display_name or self.outlet_id.name
-            weekday_label = WEEKDAY_LABELS.get(self._get_effective_weekday() or "", self._get_effective_weekday() or "")
-            self.outlet_id = False
-            response["warning"] = {
-                "title": _("Duplicate Outlet"),
-                "message": _(
-                    "Outlet %(outlet)s is already added on %(day)s in this weekly visit template. Choose another outlet for that day."
-                ) % {
-                    "outlet": outlet_name,
-                    "day": weekday_label,
-                },
-            }
-            response["domain"] = self._get_dynamic_domains()
             return response
 
         self.area_id = self.outlet_id.area_id
