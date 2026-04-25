182e33bb1dc5be259024e2de5a746db4f5943af3
yfayed2006 <yfayed2006@gmail.com>
2026-04-25 23:33:27 +0300

    Update __manifest__.py

--- addons/route_core/__manifest__.py
+++ addons/route_core/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Route Core",
-    "version": "19.0.1.0.101",
+    "version": "19.0.1.0.102",
     "summary": "Sales representative route visits",
     "author": "Yasser Fayed",
     "license": "LGPL-3",
@@ -57,6 +57,7 @@
         "views/route_role_actions_menus.xml",
         "views/route_role_ui_security_views.xml",
         "wizard/route_visit_end_wizard_views.xml",
+        "wizard/route_visit_geo_reason_wizard_views.xml",
         "wizard/route_plan_add_area_outlets_wizard_views.xml",
         "wizard/route_plan_skip_visit_wizard_views.xml",
         "wizard/route_visit_scan_wizard_views.xml",
