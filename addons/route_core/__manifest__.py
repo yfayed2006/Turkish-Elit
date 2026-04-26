3a4d7e35127fe59758e1eff032d7ae29f66efe04
yfayed2006 <yfayed2006@gmail.com>
2026-04-26 17:26:02 +0300

    Update __manifest__.py

--- addons/route_core/__manifest__.py
+++ addons/route_core/__manifest__.py
@@ -56,7 +56,6 @@
         "views/route_role_actions_menus.xml",
         "views/route_geo_review_views.xml",
         "views/route_geo_control_center_views.xml",
-        "views/route_supervisor_daily_control_views.xml",
         "views/route_role_ui_security_views.xml",
         "wizard/route_visit_end_wizard_views.xml",
         "wizard/route_plan_add_area_outlets_wizard_views.xml",
@@ -83,6 +82,7 @@
     "installable": True,
     "application": True,
 }
+
 
 
 
