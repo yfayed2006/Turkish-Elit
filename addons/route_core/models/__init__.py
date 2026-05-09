dd06e7f14b668922f162c682d31e1e4d0ee0ba12
yfayed2006 <yfayed2006@gmail.com>
2026-05-09 19:23:56 +0300

    Update __init__.py

--- addons/route_core/models/__init__.py
+++ addons/route_core/models/__init__.py
@@ -65,4 +65,6 @@ from . import route_financial_policy
 from . import route_direct_return_pricelist_policy
 
 from . import route_cheque_followup
+from . import route_custody_accounting_work_queue
+
 
