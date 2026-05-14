106902b879329913ef74fa8cd6431dffd341e96f
yfayed2006 <yfayed2006@gmail.com>
2026-05-14 15:33:44 +0300

    Delete addons/route_core/wizard/route_outlet_prospect_approval_wizard.py

--- addons/route_core/wizard/route_outlet_prospect_approval_wizard.py
+++
@@ -1,3 +0,0 @@
-# Compatibility stub.
-# The approval wizard models are intentionally defined in models/route_outlet_prospect.py
-# to avoid deployment-order issues when updating Odoo.sh from GitHub commits.
