55ccd4e04f6a7a5e3eb7ed1faf8a446cd5ddf89d
yfayed2006 <yfayed2006@gmail.com>
2026-05-15 00:23:51 +0300

    Update __init__.py

--- addons/route_core/wizard/__init__.py
+++ addons/route_core/wizard/__init__.py
@@ -24,3 +24,5 @@ from . import route_visit_whatsapp_share_wizard
 from . import route_outlet_prospect_approval_wizard
 
 # ROUTECORE_FIX_2026_05_15_0005_CONSOLIDATED_PROSPECT_FLOW_V6
+
+# ROUTECORE_FIX_2026_05_15_0110_PROSPECT_CONTEXT_WIZARD_V7
